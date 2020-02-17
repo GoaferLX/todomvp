cecb30bbe914a8ccdd4361f11c67828015518a59
GoaferLX <58623934+GoaferLX@users.noreply.github.com>
2020-02-17 19:51:06 +0000

    semi-working app

--- todos/goafgo/main.go
+++ todos/goafgo/main.go
@@ -1,31 +1,59 @@
-Package main
-Import (
-"fmt"
-"http/net"
-"Log"
-"html/template"
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
 )
 
 type ToDo struct {
-// id int
-Name string
-Completed bool
+	// id int  // want this later
+	Name      string
+	Completed bool
 }
 
-var list map[int]ToDo
-
-func (l *list) add(name string) {
-append(l, ToDo{Name: name, Completed: false}
+type List struct {
+	Items []ToDo
 }
 
-func (l *list) delete(id int) {
-delete(l, id)
-} 
-func (l *list) setCompleted(complete bool) {
-l[id].Completed = 1
+func (l *List) add(name string) {
+	l.Items = append(l.Items, ToDo{Name: name, Completed: false})
+	fmt.Println("Item added")
+}
+
+func (l *List) delete(id int) {
+	l.Items = append(l.Items[:id], l.Items[id+1:]...)
+}
+func (l *List) setCompleted(id int) {
+	l.Items[id].Completed = true
+}
+func (l *List) setNotCompleted(id int) {
+	l.Items[id].Completed = false
+}
+
+func loadTemplate(w http.ResponseWriter, tpl string, l *List) {
+	t, err := template.ParseFiles(tpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	err = t.Execute(w, l)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	l := &List{}
+	item := r.FormValue("item")
+	if r.URL.Path == "" && r.Method == "POST" {
+		l.add(item)
+	}
+
+	loadTemplate(w, "template", l)
+	fmt.Fprintln(w, "You are on the index page of ToDoMVP")
 }
 
 func main() {
-http.HandleFunc("/", handler)
-log.Fatal(http.ListenAndServe("8080", nil))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
